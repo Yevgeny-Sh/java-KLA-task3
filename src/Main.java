58dfa6f6c695ebbb40f2e29bc51accf7f18b418a
yevgeny sh <yevgst7@gmail.com>
2023-06-04 15:54:13 +0300

    seems done

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,14 @@ public class Main {
         for (Animal animal : animals) {
             animal.makeSound();
         }
-        //Each animal in the array has its own implementation of the makeSound() method
-        // and the appropriate sound is printed based on the specific animal.
+
 
         Division.divide();
 
         MySecondBankClass myAccount = new MySecondBankClass("john",1);
 
-        myAccount.deposit(100.0); // Valid deposit
-        myAccount.withdraw(50.0); // Valid withdrawal
+        myAccount.deposit(100.0);
+        myAccount.withdraw(50.0);
         myAccount.displayAccountInfo();
         myAccount.deposit(-20.0); // Invalid deposit
         myAccount.deposit(0); // Invalid deposit
@@ -29,6 +28,5 @@ public class Main {
         FilesIO.writeFile("outPut.txt","hello \nworld");
         FilesIO.readFile("outPut.txt");
         FilesIO.convertFileToUpperCase("outPut.txt");
-
     }
-    }
+}
